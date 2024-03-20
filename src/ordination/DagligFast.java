f947f23e3c26f1c6b57f5256fcf4891fb184eaef
Christoffer Ebbesen <christofferebbesen@gmail.com>
2024-03-20 11:46:45 +0100

    Faglig fast er lavet 80%

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,5 +1,59 @@
 package ordination;
 
-public class DagligFast {
-    // TODO
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+public class DagligFast extends Ordination {
+
+    double morgenAntal;
+    double middagAntal;
+    double aftenAntal;
+    double natAntal;
+    private Dosis[] dosis = new Dosis[4];
+
+    public DagligFast(Patient patient, LocalDate startDen, LocalDate slutDen, double morgenAntal, double middagAntal, double aftenAntal, double natAntal, Dosis[] dosis) {
+        super(patient, startDen, slutDen);
+        dosis[0] = ()
+        this.morgenAntal = morgenAntal;
+        this.middagAntal = middagAntal;
+        this.aftenAntal = aftenAntal;
+        this.natAntal = natAntal;
+        this.dosis = dosis;
+    }
+
+    /**
+     * Returnerer den totale dosis der er givet i den periode ordinationen er gyldig
+     *
+     * @return
+     */
+    @Override
+    public double samletDosis() {
+        return super.antalDage()*doegnDosis();
+    }
+
+    /**
+     * Returnerer den gennemsnitlige dosis givet pr dag i den periode ordinationen er gyldig
+     *
+     * @return
+     */
+    @Override
+    public double doegnDosis() {
+        double dagsdosis = 0;
+        for (Dosis doser : dosis){
+            dagsdosis += doser.getAntal();
+        }
+        return dagsdosis;
+    }
+
+    /**
+     * Returnerer ordinationstypen som en String
+     *
+     * @return
+     */
+    @Override
+    public String getType() {
+        return "Fast";
+    }
 }
